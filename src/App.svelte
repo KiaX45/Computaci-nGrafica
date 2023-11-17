005a5eee5c65e12f393dfd99f03e311582d66385
YKESHTFR <ykshtfr@gmail.com>
2023-11-17 17:54:23 -0500

    Incios

--- src/App.svelte
+++ src/App.svelte
@@ -1,63 +1,39 @@
 <script>
-	export let name;
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-
-
-
-	<div class="container">
-		<div class="row">
-			<div class="col">
-				<button type="button" class="btn btn-primary">Primary</button>
-			</div>
-			<div class="col">
-				<button type="button" class="btn btn-secondary">Secondary</button>
-			</div>
-			<div class="col">
-				<button type="button" class="btn btn-success">Success</button>
-			</div>
-			<div class="col">
-				<button type="button" class="btn btn-danger">Danger</button>
-			</div>
-			<div class="col">
-				<button type="button" class="btn btn-warning">Warning</button>
-			</div>
-			<div class="col">
-				<button type="button" class="btn btn-info">Info</button>
-			</div>
-			<div class="col">
-				<button type="button" class="btn btn-light">Light</button>
-			</div>
-			<div class="col">
-				<button type="button" class="btn btn-dark">Dark</button>
-			</div>
-			<div class="col">
-				<button type="button" class="btn btn-link">Link</button>
+	<div class="card bg-light">
+		<div class="card-body">
+			<h5 class="card-title">Planeación Simulcaro Prueba Saber Pro</h5>
+			<div class="container text-center">
+				<div class="row">
+					<div class="col">
+						<div class="form-check form-switch">
+							<input
+								class="form-check-input"
+								type="checkbox"
+								id="flexSwitchCheckDefault"
+							/>
+							<label class="form-check-label" for="flexSwitchCheckDefault"
+								>¿Enviar inivtaciones por correo?</label
+							>
+						</div>
+						<br />
+					<div class="col">
+						<div class="mb-3">
+							<label for="formFile" class="form-label"
+								>Por favor suba el archivo de la prueba</label
+							>
+							<input class="form-control" type="file" id="formFile" />
+						</div>
+					</div>
+					<div class="col">Column</div>
+				</div>
 			</div>
 		</div>
+	</div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
